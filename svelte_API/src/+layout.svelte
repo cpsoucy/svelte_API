0b32c57a1d85019bb5e6e892fa34020757e0fe76
cpsoucy <96217410+cpsoucy@users.noreply.github.com>
2023-04-20 19:26:53 -0400

    started fresh svelte project

---
+++ svelte_API/src/+layout.svelte
@@ -0,0 +1,14 @@
+<header class="header">
+	<nav class="navbar">
+		<a href="/">Home</a>
+		<a href="/recipes">Recipes</a>
+	</nav>
+</header>
+
+<slot />
+
+<footer class="footer">
+	<div class="container">
+		<p>&copy; Christian Soucy 2023<p>
+	</div>
+</footer>
---
+++ svelte_API/src/+page.svelte
@@ -0,0 +1,2 @@
+<h1><span>Random Ducks Website</span></h1>
+<p>Here on this website, you can view a random selection of duck images.</p>
