d1eb53917db70947ee765fe71f937c9ebbd0a056
cpsoucy <96217410+cpsoucy@users.noreply.github.com>
2023-04-21 19:36:31 -0400

    adding files in

---
+++ svelte_API/src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<!-- <header class="header">
+	<nav class="navbar">
+		<a href="/">Home</a>
+		<a href="/recipes">Recipes</a>
+	</nav>
+</header>
+
+<slot />
+
+<footer class="footer">
+	<div class="container">
+		<p>&copy; Christian Soucy 2023<p>
+	</div>
+</footer> -->
+
+<script>
+	// import '$svelte_API\src\files\global.css';
+	// import Nav from '$svelte_API\src\files\nav.svelte';
+	import Footer from '$svelte_API\src\files\footer.svelte';
+  </script>
+  
+  <Nav />
+  
+  <main class="container">
+	<slot />
+  </main>
+  
+  <Footer />
